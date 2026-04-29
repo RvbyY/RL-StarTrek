e9d5d6fba1f6b70a5651a517c6d59556ccb34d08
bassirou <bassirou.tall@epitech.eu>
2026-04-29 14:31:43 +0200

    add: sh exec to download all dependences and run project

---
+++ launch.sh
@@ -0,0 +1,10 @@
+sudo apt-get install -y python3 python3-pip python3-venv
+python3 -m venv ~/pytorch-env
+source ~/pytorch-env/bin/activate
+pip install --upgrade pip
+pip install numpy
+pip install torch
+pip install gymnasium
+pip install swig
+pip install gymnasium['Box2D']
+python3 src/train.py --config logs/dqn.csv
